5a140e622f0f7fdb5f319e73cef388241797fb38
TM-Tareq <tmtareq20@gmail.com>
2024-03-17 04:41:50 +0600

    calculation initialize

--- script/triangle.js
+++ script/triangle.js
@@ -1,3 +1,18 @@
 function calculateTriangleArea(){
-    console.log('inside function');
+    // Get triangle base value
+    const triangleBaseInput = document.getElementById(triangle-base);
+    const triangleBaseText = triangleBaseInput.value;
+    const base = parseFloat(triangleBaseText);
+
+    // Get triangle height
+    const triangleHeightInput = document.getElementById(triangle-height);
+    const triangleHeightText = triangleHeightInput.value;
+    const height = parseFloat(triangleHeightText);
+
+    // Calculate triangle
+    const area = 0.5 * base * height;
+
+    // Display triangle area
+    const triangleAreaSpan = document.getElementById(triangle-area);
+    triangleAreaSpan.innerText = area;
 }
